36d246a22932f844f5b8d027e1882eb3ab33169f
Aniruddha Dutta <aniruddha.dutta2@learner.manipal.edu>
2020-06-01 00:40:39 +0530

    Q1

---
+++ lab2/add_10_32bit_nums.s
@@ -0,0 +1,32 @@
+	AREA RESET,DATA,READONLY
+	EXPORT __Vectors
+__Vectors
+	DCD 0X10001000
+	DCD Reset_Handler
+	ALIGN
+	AREA mycode,CODE,READONLY
+	ENTRY
+	EXPORT Reset_Handler
+Reset_Handler
+	LDR R0,=SRC
+	LDR R5,=DST
+	LDR R1,=0     ;SUM
+	MOV R3,#10
+	MOV R4,#0  ;CARRY
+UP	
+	LDR R2,[R0],#4
+	ADDS R1,R4
+	ADC R4,#0
+	ADDS R1,R2
+	ADC R4,#0
+	subs R3,#1
+	BNE UP
+	STR R4,[R5]
+	STR R1,[R5,#4]
+HERE
+	B HERE
+SRC DCD 1,2,4,6,8,10,12,14,16,18
+	AREA mydata,DATA,READWRITE
+DST DCD 0
+	END
+	
